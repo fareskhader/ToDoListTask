9bde622e814e7e709b66b75b07a1f262eb0acccd
raed <raeddaghlas1@gmail.com>
2025-12-31 22:17:51 +0300

    add to js

---
+++ TodoList.js
@@ -0,0 +1,21 @@
+function toggle(index) {
+    todos[index].done = !todos[index].done;
+    saveTodos();
+    render();
+}
+function deleteTodo(index) {
+    currentDeleteIndex = index;
+    document.getElementById('deleteModal').style.display = 'flex';
+}
+function confirmDeleteTodo() {
+    todos.splice(currentDeleteIndex, 1);
+    closeDeleteModal();
+    msgshow("Task has been deleted.");
+    saveTodos();
+    render();
+}
+function deleteDoneTodos() {
+    todos = todos.filter(todo => !todo.done);
+    saveTodos();
+    render();
+}
